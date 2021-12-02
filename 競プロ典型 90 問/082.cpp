c878316f923fec48967ef0d99acccd447e8f707e
hal1127 <harukium@gmail.com>
2021-12-02 15:26:38 +0900

    082

--- 競プロ典型 90 問/082.cpp
+++ 競プロ典型 90 問/082.cpp
@@ -6,16 +6,38 @@ using namespace std;
 typedef long long ll;
 
 unsigned long long L, R;
+ll MOD = 1e9 + 7;
 
-unsigned long long sum(int i)
+unsigned long long pow10(int n)
 {
-  
+  unsigned long long p = 1;
+  rep (i, 0, n) {
+    p *= 10;
+    p %= MOD;
+  }
+  return p;
+}
+
+unsigned long long arith_sum(unsigned long long i)
+{
+  return (1ULL * (i+1) / 2 * i) % MOD;
+}
+
+unsigned long long digit_sum(int i)
+{
+  unsigned long long ret = arith_sum(min(pow10(i)-1, R)) - arith_sum(max(pow10(i-1), L) - 1);
+
+  return max(ret, 0ULL);
 }
 
 int main()
 {
   cin >> L >> R;
-  ll MOD = 1e9 + 7;
 
+  unsigned long long ans = 0;
+  rep (i, 1, 4) {
+    ans += digit_sum(i) % MOD;
+  }
 
+  cout << ans << endl;
 }
