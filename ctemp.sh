72fde4bd8d5ce92a4dddc5b68bda21ac71ddfd5c
hal1127 <harukium@gmail.com>
2022-01-16 22:38:24 +0900

    Update ctemp.sh

--- ctemp.sh
+++ ctemp.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-
-if [ "$(ls "$1")" ]
-then
+if [ ! -e $1 ]; then
+  mkdir $1
+fi
+if [ "$(ls $1)" ]; then
   echo "ディレクトリ内が空ではありません"
-elif [ -z "$2" ]
-then
+elif [ -z $2 ]; then
   echo "テンプレートを指定してください"
 else
   cp Template.$2 $1/A.$2
   cp Template.$2 $1/B.$2
   cp Template.$2 $1/C.$2
   cp Template.$2 $1/D.$2
-  echo "成功"
+  echo "$(ls $1)"
 fi
