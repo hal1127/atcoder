598400d4c3071e5268f90b076fb257c807b3d3b8
hal1127 <harukium@gmail.com>
2022-06-25 23:41:53 +0900

    add rrep

--- Template.cpp
+++ Template.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 #define rep(i, m, n) for(int i = m; i < n; i++)
+#define rrep(i, m, n) for (int i = m; i >= n; i--)
 #define INF 2e9
 #define all(v) v.begin(), v.end()
 using namespace std;
