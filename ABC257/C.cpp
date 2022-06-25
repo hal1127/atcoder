90ecab78e015134e635ed500381d4aebcbd268f4
hal1127 <harukium@gmail.com>
2022-06-26 01:10:09 +0900

    Update C.cpp

--- ABC257/C.cpp
+++ ABC257/C.cpp
@@ -18,8 +18,8 @@ int main()
   vector<pair<int, int>> WS(N);
   rep (i, 0, N) WS[i] = make_pair(W[i], S[i] - '0');
 
-  // ソートの仕方を工夫
-  // 同じW[i]が続く中で、端より大きいxが生まれないようにしている
+  // devise a sortting mathod.
+  // while same W[i] continues, don't be make x larger than value of edge.
   sort(all(WS), [](pair<int, int> l, pair<int, int> r) { return make_pair(l.first, -l.second) < make_pair(r.first, -r.second); });
 
   int count_adult = 0;
@@ -30,7 +30,7 @@ int main()
   rep (i, 0, N) {
     if (WS[i].second == 0) x++;
     else x--;
-    // ソートの仕方を工夫したので、以下の部分が省略可能。
+    // can omit the following part because devised a sorting method.
     // if (i < (N - 1)) {
     //   if (WS[i].first != WS[i + 1].first) ans = max(ans, x);
     // } else ans = max(ans, x);
