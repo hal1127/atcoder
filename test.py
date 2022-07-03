11dcb6730bada62005269e82060f50e76a1263a2
hal1127 <harukium@gmail.com>
2022-07-03 13:56:07 +0900

    commit

--- test.py
+++ test.py
@@ -3,7 +3,8 @@ from collections import deque
 from functools import reduce
 
 def main():
-  print("hello")
+  N = int(input())
+  A = list(map(int, input().split()))
 
 if __name__ == "__main__":
   main()
