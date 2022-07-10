f5c415f004988bd46016200df49ed0b99cc1038d
hal1127 <harukium@gmail.com>
2022-07-10 15:55:54 +0900

    Update cretemp.sh

--- cretemp.sh
+++ cretemp.sh
@@ -3,9 +3,7 @@ if [ ! -e $1 ]; then
   mkdir $1
 fi
 
-if [ "$(ls $1)" ]; then
-  echo "ディレクトリ内が空ではありません"
-elif [ -z $2 ]; then
+if [ -z $2 ]; then
   echo "テンプレートを指定してください"
 elif [ $2 = "java" ]; then
   echo "java 専用ディレクトリ構造\n"
@@ -13,18 +11,18 @@ elif [ $2 = "java" ]; then
   mkdir $1/B
   mkdir $1/C
   mkdir $1/D
-  cp Template.$2 $1/A/Main.$2
-  cp Template.$2 $1/B/Main.$2
-  cp Template.$2 $1/C/Main.$2
-  cp Template.$2 $1/D/Main.$2
+  cp -n Template.$2 $1/A/Main.$2
+  cp -n Template.$2 $1/B/Main.$2
+  cp -n Template.$2 $1/C/Main.$2
+  cp -n Template.$2 $1/D/Main.$2
   echo "$(tree $1)"
-elif [ -n $3 ]; then
-  cp Template.$2 $1/$3.$2
+elif [ $# = 3 ]; then
+  cp -n Template.$2 $1/$3.$2
   echo "$(tree $1)"
 else
-  cp Template.$2 $1/A.$2
-  cp Template.$2 $1/B.$2
-  cp Template.$2 $1/C.$2
-  cp Template.$2 $1/D.$2
+  cp -n Template.$2 $1/A.$2
+  cp -n Template.$2 $1/B.$2
+  cp -n Template.$2 $1/C.$2
+  cp -n Template.$2 $1/D.$2
   echo "$(tree $1)"
 fi
