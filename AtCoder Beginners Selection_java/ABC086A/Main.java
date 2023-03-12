84cf8c648e6cbcfedfd01c0f3e7fe49dc3248d69
hal1127 <harukium@gmail.com>
2023-03-13 01:07:05 +0900

    commit

--- AtCoder Beginners Selection_java/ABC086A/Main.java
+++ AtCoder Beginners Selection_java/ABC086A/Main.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
   public static void main (String[] args)
@@ -9,6 +10,13 @@ public class Main {
   void run ()
   {
     Scanner cin = new Scanner(System.in);
-    
+    List<String> list = Arrays.asList(cin.nextLine().split(" "));
+    List<Integer> list2 = list.stream().map(x -> Integer.parseInt(x)).collect(Collectors.toList());
+
+    if (list2.get(0) * list2.get(1) % 2 == 1) {
+      System.out.println("Odd");
+    } else {
+      System.out.println("Even");
+    }
   }
 }
