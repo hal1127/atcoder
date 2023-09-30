141c76f56bbc643025429cce19e55f3d01baa521
hal1127 <harukium@gmail.com>
2023-09-30 15:32:21 +0900

    commit

--- AtCoder Beginners Selection_java/ABC081B/Main.java
+++ AtCoder Beginners Selection_java/ABC081B/Main.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
   public static void main (String[] args)
@@ -9,6 +10,24 @@ public class Main {
   void run ()
   {
     Scanner cin = new Scanner(System.in);
-    
+    Integer n = Integer.parseInt(cin.nextLine());
+    List<String> list = Arrays.asList(cin.nextLine().split(" "));
+    List<Integer> a = list.stream().map(x -> Integer.parseInt(x)).collect(Collectors.toList());
+
+    Boolean isFinish = false;
+    Integer cnt = 0;
+    while (!isFinish) {
+      for (Integer i = 0; i < n; i++) {
+        if (a.get(i) % 2 == 0) {
+          a.set(i, a.get(i) / 2);
+        } else {
+          isFinish = true;
+        }
+      }
+      if (!isFinish) {
+        cnt++;
+      }
+    }
+    System.out.println(cnt);
   }
 }
--- AtCoder Beginners Selection_java/ABC087B/Main.java
+++ AtCoder Beginners Selection_java/ABC087B/Main.java
@@ -3,10 +3,10 @@ import java.util.*;
 public class Main {
   public static void main (String[] args)
   {
-    new Main().run();
+    Main.run();
   }
 
-  void run ()
+  static void run ()
   {
     Scanner cin = new Scanner(System.in);
     
